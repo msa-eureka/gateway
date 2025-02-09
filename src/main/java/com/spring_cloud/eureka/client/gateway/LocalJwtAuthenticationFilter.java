446aa93215a0926fb71e42b3164a99c3d8bcbf2b
je-pa <jieun0502@gmail.com>
2025-02-10 00:04:36 +0900

    [#2] 보안구성 - 토큰 검증 필터 추가

---
+++ src/main/java/com/spring_cloud/eureka/client/gateway/LocalJwtAuthenticationFilter.java
@@ -0,0 +1,80 @@
+package com.spring_cloud.eureka.client.gateway;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+@Slf4j
+@Component
+public class LocalJwtAuthenticationFilter implements GlobalFilter {
+
+  @Value("${service.jwt.secret-key}")
+  private String secretKey;
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    String path = exchange.getRequest().getURI().getPath();
+    if (path.equals("/auth/signIn")) {
+      return chain.filter(exchange);  // /signIn 경로는 필터를 적용하지 않음
+    }
+
+    String token = extractToken(exchange);
+
+    if (token == null || !validateToken(token)) {
+      exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+      return exchange.getResponse().setComplete();
+    }
+
+    return chain.filter(exchange);
+  }
+
+  private String extractToken(ServerWebExchange exchange) {
+    String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      return authHeader.substring(7);
+    }
+    return null;
+  }
+
+  private boolean validateToken(String token) {
+    try {
+      SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+      Jws<Claims> claimsJws = Jwts.parser()
+          .verifyWith(key)
+          .build().parseSignedClaims(token);
+      log.info("#####payload :: " + claimsJws.getPayload().toString());
+
+      // 추가적인 검증 로직 (예: 토큰 만료 여부 확인 등)을 여기에 추가할 수 있습니다.
+      return true;
+    } catch (SecurityException | MalformedJwtException | SignatureException e) {
+      log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+    } catch (ExpiredJwtException e) {
+      log.error("Expired JWT token, 만료된 JWT token 입니다.");
+    } catch (UnsupportedJwtException e) {
+      log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+    } catch (IllegalArgumentException e) {
+      log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+    } catch (Exception e) {
+      log.error("Exception");
+    }
+    return false;
+  }
+
+
+
+}
